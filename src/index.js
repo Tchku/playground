f8fb9b0c210d32c00f8c44e9694ae3dd26978d41
Tchku <indre.kocyte@gmail.com>
2024-01-07 16:16:38 +0200

    Deployment

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import About from "./pages/About";
 
 export default function Root() {
   return (
-    <BrowserRouter basename="/playground">
+    <BrowserRouter basename="/home">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
